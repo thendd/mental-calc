7a27c51f0b752469fd92cf616dfd87d14ae7e041
aktshually <end313234@protonmail.com>
2025-09-24 17:03:50 -0300

    feat: verify whether the given answer is correct

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,47 @@
 <template>
-  <NuxtWelcome />
+  <UHeader title="Mental math" />
+  <UMain>
+    <h2>
+      {{ currentOperation.left }} {{ currentOperation.sign }}
+      {{ currentOperation.right }} = x
+    </h2>
+    <UForm @submit.prevent="onSubmit">
+      <UFormField>
+        <UInput
+          v-model="givenAnswer"
+          type="number"
+        />
+      </UFormField>
+      <UButton
+        type="submit"
+        label="Guess"
+      />
+    </UForm>
+  </UMain>
 </template>
+
+<script setup lang="ts">
+type Operation = {
+  left: number
+  right: number
+  sign: string
+  answer: number
+}
+
+const currentOperation: Operation = {
+  left: 3,
+  right: 2,
+  sign: '+',
+  answer: 3 + 2,
+}
+
+const givenAnswer = ref<number>()
+
+const isGivenAnswerCorrect = () => {
+  return givenAnswer.value == currentOperation.answer
+}
+
+const onSubmit = () => {
+  alert(isGivenAnswerCorrect() ? 'correct' : 'wrong loser')
+}
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/ui', '@nuxt/eslint'],
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
 })
