c696da0d2bcfc95e7131a9d7a9004bd25f2bc46f
aktshually <end313234@protonmail.com>
2025-09-26 19:54:56 -0300

    feat: add addition

--- app/app.vue
+++ app/app.vue
@@ -2,21 +2,10 @@
   <UHeader title="Mental math" />
   <UMain>
     <h2>
-      {{ currentOperation.left }} {{ currentOperation.sign }}
-      {{ currentOperation.right }} = x
+      {{ currentOperation?.left }} {{ currentOperation?.sign }}
+      {{ currentOperation?.right }} = x
     </h2>
-    <UForm @submit.prevent="onSubmit">
-      <UFormField>
-        <UInput
-          v-model="givenAnswer"
-          type="number"
-        />
-      </UFormField>
-      <UButton
-        type="submit"
-        label="Guess"
-      />
-    </UForm>
+    <input v-model="givenAnswer" :oninput="onChange" />
   </UMain>
 </template>
 
@@ -26,22 +15,71 @@ type Operation = {
   right: number
   sign: string
   answer: number
+  worth: number
 }
 
-const currentOperation: Operation = {
-  left: 3,
-  right: 2,
-  sign: '+',
-  answer: 3 + 2,
+let grade = 1
+
+const generateRandomNumber = (min: number, max: number) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-const givenAnswer = ref<number>()
+const newOperation = () => {
+  const operationSigns = ['+', '-', 'x', '/']
+  let left = 0
+  let right = 0
+  let answer = 0
+  let worth = 0
+  let operationSign = '+'
 
-const isGivenAnswerCorrect = () => {
-  return givenAnswer.value == currentOperation.answer
+  if (grade < 1.5) {
+    left = generateRandomNumber(0, 10)
+    right = generateRandomNumber(0, 10)
+
+    operationSign = operationSigns[generateRandomNumber(0, 1)] as string
+    worth = 0.1
+  }
+
+  switch (operationSign) {
+    case '+':
+      answer = left + right
+      break
+    case '-':
+      answer = left - right
+      break
+    case 'x':
+      answer = left * right
+      break
+    case '/':
+      answer = left / right
+  }
+
+  return {
+    left,
+    right,
+    answer,
+    worth,
+    sign: operationSign,
+  }
 }
 
-const onSubmit = () => {
-  alert(isGivenAnswerCorrect() ? 'correct' : 'wrong loser')
+const currentOperation = ref<Operation>()
+
+const givenAnswer = ref<number | undefined>()
+
+const onChange = (event: InputEvent) => {
+  const { value } = event.target as HTMLInputElement
+  console.log(Number(value), currentOperation.value?.answer)
+  if (Number(value) === currentOperation.value?.answer) {
+    grade += currentOperation.value.worth
+    currentOperation.value = newOperation()
+    givenAnswer.value = undefined
+  }
 }
+
+onMounted(() => {
+  currentOperation.value = newOperation()
+})
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui', '@nuxt/eslint'],
+  modules: ['@nuxt/ui', '@nuxt/eslint', '@vueuse/nuxt'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
